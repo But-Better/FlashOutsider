71b6d059d803b3ac7ec1e9f10f52189297095041
note <kitsu-geissler@t-online.de>
2022-01-05 23:10:46 +0100

    reformat remove_build_zephyr_directory.py

--- python_scripts/flash_preparation/rework_scripts/remove_build_zephyr_directory.py
+++ python_scripts/flash_preparation/rework_scripts/remove_build_zephyr_directory.py
@@ -1,8 +1,10 @@
 import shutil
 import os
 
+
 def run(build_path):
     if not os.path.exists(f"{build_path}/zephyr"):
         print(f"the remove of {build_path}/zephyr was not necessary, since it didn't exist")
+        exit()
 
     shutil.rmtree(f"{build_path}/zephyr")
