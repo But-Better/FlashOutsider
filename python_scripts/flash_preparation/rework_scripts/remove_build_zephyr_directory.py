f966f528175f49e4e3a2d7a234b4573aef5a0e44
note <kitsu-geissler@t-online.de>
2022-01-05 23:11:18 +0100

    add return on no zephyr directory found

--- python_scripts/flash_preparation/rework_scripts/remove_build_zephyr_directory.py
+++ python_scripts/flash_preparation/rework_scripts/remove_build_zephyr_directory.py
@@ -5,6 +5,6 @@ import os
 def run(build_path):
     if not os.path.exists(f"{build_path}/zephyr"):
         print(f"the remove of {build_path}/zephyr was not necessary, since it didn't exist")
-        exit()
+        return
 
     shutil.rmtree(f"{build_path}/zephyr")
